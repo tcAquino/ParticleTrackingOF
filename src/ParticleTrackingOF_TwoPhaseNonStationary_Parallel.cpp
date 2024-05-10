e86b7654401cac822f2be55604cc4c3b80d5c147
tcAquino <tomas.c.aquino@gmail.com>
2024-05-10 13:06:33 +0200

    Remove -WNo-undefined-var-template compiler flag for gcc

--- src/ParticleTrackingOF_TwoPhaseNonStationary_Parallel.cpp
+++ src/ParticleTrackingOF_TwoPhaseNonStationary_Parallel.cpp
@@ -23,7 +23,7 @@
 
 int main(int argc, char * argv[])
 {
-  using namespace ptof::model_bcc_symmetryplanes_advection_diffusion_parallel;
+  using namespace ptof::model_advection_diffusion_2d_parallel;
   using Phase = ptof::Phase;
   
   std::string banner =
